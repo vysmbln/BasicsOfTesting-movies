a0d7bcbd61a8f7939a13d5f8e2934ce866c7170b
vysmbln <vysmbln@users.noreply.github.com>
2020-06-24 17:16:07 -0400

    final update

--- test/autocomplete.test.js
+++ test/autocomplete.test.js
@@ -1,19 +1,62 @@
-it('Shows an autocomplete', () => {
-    createAutoComplete({
-        root: document.querySelector('#target'),
-        fetchData() {
-            return [
-                {Title: 'Avengers' },
-                {Title: 'Parasite' },
-                {Title: 'Rush Hour' }
-            ];
-        },
-        renderOption(movie) {
-            return movie.Title;
+const waitFor = selector => {
+    return new Promise((resolve, reject) => {
+      const interval = setInterval(() => {
+        if (document.querySelector(selector)) {
+          clearInterval(interval);
+          clearTimeout(timeout);
+          resolve();
         }
+      }, 30);
+  
+      const timeout = setTimeout(() => {
+        clearInterval(interval);
+        reject();
+      }, 2000);
     });
+  };
+  
+  beforeEach(() => {
+    document.querySelector('#target').innerHTML = '';
+    createAutoComplete({
+      root: document.querySelector('#target'),
+      fetchData() {
+        return [
+          { Title: 'Avengers' },
+          { Title: 'Not Avengers' },
+          { Title: 'Some other movie' }
+        ];
+      },
+      renderOption(movie) {
+        return movie.Title;
+      }
+    });
+  });
+  
+  it('Dropdown starts closed', () => {
     const dropdown = document.querySelector('.dropdown');
+  
+    expect(dropdown.className).not.to.include('is-active');
+  });
+  
+  it('After searching, dropdown opens up', async () => {
+    const input = document.querySelector('input');
+    input.value = 'avengers';
+    input.dispatchEvent(new Event('input'));
+  
+    await waitFor('.dropdown-item');
+  
+    const dropdown = document.querySelector('.dropdown');
+    expect(dropdown.className).to.include('is-active');
+  });
+  
+  it('After searching, display some results', async () => {
+    const input = document.querySelector('input');
+    input.value = 'avengers';
+    input.dispatchEvent(new Event('input'));
+  
+    await waitFor('.dropdown-item');
 
-   
+    const items = document.querySelectorAll('.dropdown-item');
 
-});
+    expect(items.length).to.equal(3);
+  })
